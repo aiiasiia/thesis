5f8a9114d49419702386683e74d2397fea7f9f86
Aia Sia <aia.sia1995@gmail.com>
2015-11-14 09:47:39 +0800

    Shell script worked!

--- code/test.sh
+++ code/test.sh
@@ -21,7 +21,8 @@ done
 cd ..
 git add --all .
 git commit -m 'Shell script worked!'
-while[ -n 'git status --porcelain' ] do
+s=git status --porcelain
+while[ -n s ] do
 	echo "git push to aiiasiia/thesis"
 	git push --repo https://aiiasiia:ghh3lln0@github.com/aiiasiia/thesis > log
 done
